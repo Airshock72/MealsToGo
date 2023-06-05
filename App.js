cb0742f3abb040d2279e065771de37125f7750d7
Airshock72 <gelagela2222@gmail.com>
2023-06-05 21:49:36 +0400

    added fonts

--- App.js
+++ App.js
@@ -4,8 +4,22 @@ import { SafeAreaProvider } from "react-native-safe-area-view";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./src/infrastructure/theme";
+import { useFonts, Oswald_400Regular } from "@expo-google-fonts/oswald";
+import { Lato_400Regular } from "@expo-google-fonts/lato";
 
 export default function App() {
+  const [oswaldLoaded] = useFonts({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useFonts({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <SafeAreaProvider>
       <ThemeProvider theme={theme}>
