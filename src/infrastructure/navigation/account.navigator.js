334000afc7fc4906ae733c5d8d49036363cb91f4
Airshock72 <gelagela2222@gmail.com>
2023-06-26 23:43:00 +0400

    added login register and account screens also added bg image

--- src/infrastructure/navigation/account.navigator.js
+++ src/infrastructure/navigation/account.navigator.js
@@ -1,28 +1,20 @@
 import React from "react";
 
 import { createStackNavigator } from "@react-navigation/stack";
-import { Text, View } from "react-native";
+import { AccountScreen } from "../../features/account/screens/account.screen";
+import { LoginScreen } from "../../features/account/screens/login.screen";
+import { RegisterScreen } from "../../features/account/screens/register.screen";
 
 const Stack = createStackNavigator();
 
-export const AccountNavigator = () => (
-  <Stack.Navigator>
-    <Stack.Screen
-      name="Main"
-      component={() => (
-        <View>
-          <Text>Account Screen</Text>
-        </View>
-      )}
-    />
+const screenOptions = () => ({
+  headerShown: false,
+});
 
-    <Stack.Screen
-      name="Login"
-      component={() => (
-        <View>
-          <Text>Login Screen</Text>
-        </View>
-      )}
-    />
+export const AccountNavigator = () => (
+  <Stack.Navigator screenOptions={screenOptions}>
+    <Stack.Screen name="Main" component={AccountScreen} />
+    <Stack.Screen name="Login" component={LoginScreen} />
+    <Stack.Screen name="Register" component={RegisterScreen} />
   </Stack.Navigator>
 );
