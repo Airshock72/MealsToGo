6851ac0c68b7024175acd773f4d9a37603da61be
Airshock72 <gelagela2222@gmail.com>
2023-06-26 18:29:01 +0400

    added firebase config to project

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-view";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./src/infrastructure/theme";
@@ -10,8 +10,40 @@ import { RestaurantsContextProvider } from "./src/services/restaurants/restauran
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { Navigation } from "./src/infrastructure/navigation";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDX2FA_NsSc9IiKfn_oc-DkquMcLt6P6S8",
+  authDomain: "mealstogo-d6f93.firebaseapp.com",
+  projectId: "mealstogo-d6f93",
+  storageBucket: "mealstogo-d6f93.appspot.com",
+  messagingSenderId: "559327783575",
+  appId: "1:559327783575:web:1e4bf1e49750556319d7d2",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const auth = firebase.auth();
 
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    setTimeout(
+      auth
+        .signInWithEmailAndPassword("email@gmail.com", "test123")
+        .then((user) => {
+          setIsAuthenticated(true);
+        })
+        .catch((err) => console.log(err)),
+      2000
+    );
+  }, []);
+
   const [oswaldLoaded] = useFonts({
     Oswald_400Regular,
   });
@@ -21,6 +53,10 @@ export default function App() {
   });
 
   if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
+  if (!isAuthenticated) {
     return null;
   }
 
