81989b67243287d88001fd2172222d56c83ce129
Airshock72 <gelagela2222@gmail.com>
2023-06-28 10:28:53 +0400

    added lottie library and added watermelon animation to accoutn screen

--- src/components/utility/safe-area.component.jsx
+++ src/components/utility/safe-area.component.jsx
@@ -3,4 +3,5 @@ import SafeAreaView from "react-native-safe-area-view";
 
 export const SafeArea = styled(SafeAreaView)`
   flex: 1;
+  background-color: ${(props) => props.theme.colors.bg.primary};
 `;
--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -3,17 +3,27 @@ import {
   AccountBackground,
   AccountContainer,
   AccountCover,
+  AnimationWrapper,
   AuthButton,
   Title,
 } from "../components/account.styles";
 import { Spacer } from "../../../components/spacer/spacer.component";
 import { AuthenticationContext } from "../../../services/authentication/authentication.context";
+import AnimatedLottieView from "lottie-react-native";
 
 export const AccountScreen = ({ navigation }) => {
   const { setError } = useContext(AuthenticationContext);
   return (
     <AccountBackground>
       <AccountCover />
+      <AnimationWrapper>
+        <AnimatedLottieView
+          autoPlay
+          loop
+          resizeMode="cover"
+          source={require("../../../../assets/watermelon.json")}
+        />
+      </AnimationWrapper>
       <Title>Meals To Go</Title>
       <AccountContainer>
         <AuthButton
