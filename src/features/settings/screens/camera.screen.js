1729300e668cb8f44cd8d79e8b49835733c28e84
Airshock72 <gelagela2222@gmail.com>
2023-07-03 19:26:37 +0400

    added and fixed camera resolution button

--- src/features/settings/screens/camera.screen.js
+++ src/features/settings/screens/camera.screen.js
@@ -1,17 +1,39 @@
 import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components/native";
-import { Camera } from "expo-camera";
+import { Camera, CameraType } from "expo-camera";
 import { View } from "react-native";
 import { Text } from "../../../components/typography/text.component";
+import { Button } from "react-native-paper";
 
 const ProfileCamera = styled(Camera)`
   width: 100%;
   height: 100%;
 `;
 
+const CameraContainer = styled.View`
+  width: 100%;
+  height: 100%;
+`;
+
+const CameraButton = styled(Button).attrs({
+  mode: "contained",
+  icon: "camera",
+})`
+  position: absolute;
+  top: 525px;
+  left: 165px;
+`;
+
 export const CameraScreen = () => {
   const [hasPermission, setHasPermission] = useState(null);
   const cameraRef = useRef();
+
+  const snap = async () => {
+    if (cameraRef) {
+      const photo = await cameraRef.current.takePictureAsync();
+      console.log(photo);
+    }
+  };
 
   useEffect(() => {
     (async () => {
@@ -29,9 +51,17 @@ export const CameraScreen = () => {
   }
 
   return (
-    <ProfileCamera
-      ref={(camera) => (cameraRef.current = camera)}
-      type={Camera.Constants.Type.front}
-    />
+    <CameraContainer>
+      <ProfileCamera
+        ref={(camera) => (cameraRef.current = camera)}
+        ratio={"16:9"}
+        type={CameraType.front}
+        onCameraReady={() => {
+          console.log("Camera Ready");
+        }}
+      />
+
+      <CameraButton onPress={snap}>Snap!</CameraButton>
+    </CameraContainer>
   );
 };
